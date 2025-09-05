0f7391a0e06fc629f2c90412cc5f06d08259ce03
Drishti-tech2507 <drishtic250706@gmail.com>
2025-09-05 11:50:23 +0530

    Time: 1 ms (38.46%), Space: 40.6 MB (89.23%) - LeetHub

---
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        for(int k = 1; k <= 60; k++)
+        {
+            long remain = (long)num1 - (long)k * num2;
+            if(remain < 0)continue;
+            int bits = Long.bitCount(remain);
+            if(bits <= k && k <= remain)
+            {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
