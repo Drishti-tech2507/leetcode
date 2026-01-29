38cdb9dac6b2c9108f4e8bc6bf339694c8606ee4
Drishti-tech2507 <drishtic250706@gmail.com>
2026-01-29 13:36:04 +0530

    Time: 1890 ms (5%), Space: 55.8 MB (41.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0; // safe default because majority is guaranteed to exist
-        for (int num : nums) {
-            if (count == 0) candidate = num;
-            count += (num == candidate) ? 1 : -1;
+        int n = nums.length;
+        for(int i = 0; i < n; i++)
+        {
+            int c = 0;
+            for(int j = 0; j < n; j++)
+            {
+                if(nums[j] == nums[i])
+                {
+                    c++;
+                }
+            }
+            if(c > (n / 2))
+            {
+                return nums[i];
+            }
         }
-        return candidate;
-    }
-
-    public static void main(String[] args) {
-        int[] nums1 = {3, 2, 3};
-        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
-
-        // Create an object to call the non-static method
-        Solution obj = new Solution();
-
-        System.out.println("Majority Element in nums1: " + obj.majorityElement(nums1)); // 3
-        System.out.println("Majority Element in nums2: " + obj.majorityElement(nums2)); // 2
+        return -1;
     }
 }
