16d9e5bc4f2461ac664f2edf701479f823c4b74b
Drishti-tech2507 <drishtic250706@gmail.com>
2026-02-03 22:36:53 +0530

    Time: 0 ms (100%), Space: 43 MB (75.03%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,38 +1,39 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer>ans = new ArrayList<>();
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int top = 0, left = 0, bottom = n - 1, right = m - 1;
-        while(top <= bottom && left <= right)
+       List<Integer> ans = new ArrayList<>();
+       int n = matrix .length;
+       int m = matrix[0].length;
+       int t = 0, left = 0;
+       int b = n - 1, right = m -1;
+       while(t <= b && left <= right)
+       {
+        for(int i = left; i <= right; ++i)
         {
-            for(int i = left; i <= right; i++)
-            {
-                ans.add(matrix[top][i]);
-            }
-            top++;
-            for(int i = top; i<= bottom; i++)
-            {
-                ans.add(matrix[i][right]);
-            }
-            right--;
-            if(top <= bottom)
-            {
-                for(int i = right; i >= left; i--)
-                {
-                    ans.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            if(left <= right)
-            {
-                for(int i = bottom; i >=top; i--)
-                {
-                    ans.add(matrix[i][left]);
-                }
-                left++;
-            }
+            ans.add(matrix[t][i]);
         }
-        return ans;
+        t++;
+        for(int i = t; i <= b; ++i)
+        {
+            ans.add(matrix[i][right]);
+        }
+        right--;
+        if(t <= b)
+        {
+            for(int i = right; i >= left; --i)
+            {
+                ans.add(matrix[b][i]);
+            }
+            b--;
+        }
+        if(left <= right)
+        {
+            for(int i = b; i >= t; --i)
+            {
+                ans.add(matrix[i][left]);
+            }
+            left++;
+        }
+       }
+       return ans;
     }
 }
