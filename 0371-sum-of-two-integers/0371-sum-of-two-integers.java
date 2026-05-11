8003c26e9abab13f3411014f2307273c69a0394b
Drishti Chopra <drishtic250706@gmail.com>
2026-05-11 20:38:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.17%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int n;
+        return a + b;
+    }
+    public static void main(String[] args)
+{
+   int n = 5;
+   System.out.println(n);
+}
+
+}
