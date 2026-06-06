2fa172505cb70be6f6480accf5313964c409fa1f
Drishti Chopra <drishtic250706@gmail.com>
2026-06-06 09:58:51 +0530

    Time: 0 ms (100%), Space: 44.1 MB (87.97%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -13,20 +13,21 @@ class Solution {
         if(head == null || head.next == null || k == 0)
         return head;
         ListNode curr = head;
-        int length = 1;
-        while (curr.next != null) {
+        int l = 1;
+        while(curr.next != null)
+        {
             curr = curr.next;
-            length++;
+            l++;
         }
         curr.next = head;
-        k = k % length;
-        int stepsToNewHead = length - k;
-        ListNode newTail = curr;
-        while (stepsToNewHead-- > 0) {
-            newTail = newTail.next;
+        k = k % l;
+        int sTN = l - k;
+        ListNode newTrail = curr;
+        while(sTN-- > 0){
+        newTrail = newTrail.next;
         }
-        ListNode newHead = newTail.next;
-        newTail.next = null;
+        ListNode newHead = newTrail.next;
+        newTrail.next = null;
         return newHead;
     }
 }
